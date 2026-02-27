f90b2fa3d9eeedb1bbc2d58c2f6ea2c66bdd1a67
janvi <janviyadav120505@gmail.com>
2026-02-27 11:43:13 +0530

    url

--- URL/index.js
+++ URL/index.js
@@ -1,16 +1,23 @@
 const http = require("http");
 const fs= require("fs");
+const url=require("url"); //we use this coz this help us in getting all the things about a url 
+
 
 //how we analise url
 
 const myserver=http.createServer((req,res)=>{ 
-    const log=`${Date.now()}:${req.url}New request\n`     
+    const log=`${Date.now()}:${req.url}New request\n` 
+    const myurl=url.parse(req.url,true)   //true will make the parsestring go true
+    console.log(myurl) 
     fs.appendFile('./log.txt',log,(err,data)=>{
-         switch(req.url)
+         switch(myurl.pathname)
          {
             case '/':res.end("Homepage");
             break
-            case '/about': res.end("My name is janvi");
+            case '/about':
+                const username=myurl.query.myname
+                 res.end(`My name is${username}`);
+            break     
             default: res.end("Welcome to my server")
          }
 
