a670bbd74055d7da5d078c830bd624dc676f6c83
Janvi Yadav <janviyadav120505@gmail.com>
2026-09-11 19:04:46 +0530

    implemented validation and authentication imlpemented Zod

--- project-2/controllers/auth.js
+++ project-2/controllers/auth.js
@@ -1,23 +1,42 @@
-const User=require("../model/user")
+const User = require("../model/user");
+const bcrypt = require("bcrypt");
 
-const registerUser=async(req,res)=>{
-    try{
-        const {name,email,password,role}=req.body;
 
-        console.log(name,email,password,role);
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password, role } = req.body;
 
-         res.status(201).json({
-            message: "Registration request received"
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(409).json({
+                message: "User with this email already exists"
+            });
+        }
+
+        const hashedpassword = await bcrypt.hash(password, 12);
+
+
+        const user=  await User.create({
+            name,
+            email,
+            password:hashedpassword,
+            role
+        })
+
+        console.log(name, email, password, role);
+
+        res.status(201).json({
+            message: "User registered successfully"
         });
 
+    } catch (error) {
+    console.log(error);
 
-    }
-    catch (error) {
-        res.status(500).json({
-            message: "Server error"
-        });
-    }
-
-   
+    res.status(500).json({
+        message: "Server error"
+    });
 }
- module.exports={registerUser}
+};
+
+module.exports = { registerUser };
